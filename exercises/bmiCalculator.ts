1ff8174cc0e3e2aa0dae81a75736ec48890b3df0
jcLucio <c.lucio3@gmail.com>
2021-07-29 13:05:06 -0500

    Add WebBMI exercise (9.5) - Move calculateBmi function into calculateBmi - Add /bmi endpoint

--- exercises/bmiCalculator.ts
+++ exercises/bmiCalculator.ts
@@ -1,3 +1,5 @@
+import calculateBmi from './calculateBmi';
+
 interface BmiValues {
   height: number;
   weight: number;
@@ -14,43 +16,6 @@ const parseBmiArguments = (args: Array<string>): BmiValues => {
     };
   } else {
     throw new Error('Provided values were not numbers!');
-  }
-};
-
-type BmiResult =
-  | 'Invalid bmi value'
-  | 'Underweight (severe thinness)'
-  | 'Underweight (moderate thinness)'
-  | 'Underweight (mild thinness)'
-  | 'Normal (healthy weight)'
-  | 'Overweight (pre-obese)'
-  | 'Obese (class I)'
-  | 'Obese (class II)'
-  | 'Obese (class III)';
-
-const calculateBmi = (height: number, weight: number): BmiResult => {
-  const bmi = weight / (height / 100) ** 2;
-
-  switch (true) {
-    case bmi < 16:
-      return 'Underweight (severe thinness)';
-    case 16 <= bmi && bmi < 17:
-      return 'Underweight (moderate thinness)';
-    case 17 <= bmi && bmi < 18.5:
-      return 'Underweight (mild thinness)';
-    case 18.5 <= bmi && bmi < 24.9:
-      return 'Normal (healthy weight)';
-    case 25 <= bmi && bmi < 30:
-      return 'Overweight (pre-obese)';
-    case 30 <= bmi && bmi < 35:
-      return 'Obese (class I)';
-    case 35 <= bmi && bmi < 40:
-      return 'Obese (class II)';
-    case 40 <= bmi:
-      return 'Obese (class III)';
-
-    default:
-      throw new Error('Invalid bmi values');
   }
 };
 
--- exercises/index.ts
+++ exercises/index.ts
@@ -1,11 +1,35 @@
 import express from 'express';
+import calculateBmi from './calculateBmi';
 const app = express();
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  try {
+    const height = Number(req.query.height);
+    const weight = Number(req.query.weight);
+
+    if (isNaN(height) || isNaN(weight)) {
+      throw new Error('malformatted parameters');
+    }
+
+    const bmi = calculateBmi(height, weight);
+
+    const bmiResult = {
+      height,
+      weight,
+      bmi,
+    };
+
+    res.json(bmiResult);
+  } catch (e) {
+    res.status(400).json({ error: e.message });
+  }
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
