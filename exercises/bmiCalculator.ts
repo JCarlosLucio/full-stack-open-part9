41160d28cb8f704594a5476b0d7f8f5cc055e118
jcLucio <c.lucio3@gmail.com>
2021-07-28 20:56:49 -0500

    Add body mass index exercise (9.1) - Install ts-node typescript - Add .gitignore w/ node_modules - Add tsconfig.json w/ noImplicitAny - Add ts-node / calculateBmi scripts - Add bmiCalculator.ts w/calculateBmi

---
+++ exercises/bmiCalculator.ts
@@ -0,0 +1,38 @@
+type Result =
+  | 'Invalid bmi value'
+  | 'Underweight (severe thinness)'
+  | 'Underweight (moderate thinness)'
+  | 'Underweight (mild thinness)'
+  | 'Normal (healthy weight)'
+  | 'Overweight (pre-obese)'
+  | 'Obese (class I)'
+  | 'Obese (class II)'
+  | 'Obese (class III)';
+
+const calculateBmi = (height: number, weight: number): Result => {
+  const bmi = weight / (height / 100) ** 2;
+
+  switch (true) {
+    case bmi < 16:
+      return 'Underweight (severe thinness)';
+    case 16 <= bmi && bmi < 17:
+      return 'Underweight (moderate thinness)';
+    case 17 <= bmi && bmi < 18.5:
+      return 'Underweight (mild thinness)';
+    case 18.5 <= bmi && bmi < 24.9:
+      return 'Normal (healthy weight)';
+    case 25 <= bmi && bmi < 30:
+      return 'Overweight (pre-obese)';
+    case 30 <= bmi && bmi < 35:
+      return 'Obese (class I)';
+    case 35 <= bmi && bmi < 40:
+      return 'Obese (class II)';
+    case 40 <= bmi:
+      return 'Obese (class III)';
+
+    default:
+      return 'Invalid bmi value';
+  }
+};
+
+console.log(calculateBmi(180, 74));
