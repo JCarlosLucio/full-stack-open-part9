9298b51f5448b147fd2cb782477a84c7ae69f284
jcLucio <c.lucio3@gmail.com>
2021-07-30 17:03:56 -0500

    Add patientor backend step3 exercise (9.10) - Move index.ts into src/ - Update dev script to use src/index.ts - Add Diagnose type - Add diagnoses data - Add diagnoseService w/ getDiagnoses - Add diagnoseRouter w/ /api/diagnoses endpoint

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
+
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -10,6 +12,8 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
