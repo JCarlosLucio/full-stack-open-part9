8f9a034b31e5a28dc7bcbef10e914c8f7c124e82
jcLucio <c.lucio3@gmail.com>
2021-07-29 23:00:16 -0500

    Add WebExercises exercise (9.7) - Move calculateExercises function to calculateExercises.ts - Add exercises.rest request for testing - Add /exercises endpoint

--- exercises/exerciseCalculator.ts
+++ exercises/exerciseCalculator.ts
@@ -1,3 +1,5 @@
+import calculateExercises from './calculateExercises';
+
 interface ExercisesValues {
   target: number;
   hours: Array<number>;
@@ -15,42 +17,6 @@ const parseArguments = (args: Array<string>): ExercisesValues => {
   } else {
     throw new Error('Provided values were not numbers!');
   }
-};
-
-interface Result {
-  periodLength: number;
-  trainingDays: number;
-  success: boolean;
-  rating: number;
-  ratingDescription: string;
-  target: number;
-  average: number;
-}
-
-const calculateExercises = (target: number, hours: Array<number>): Result => {
-  const periodLength = hours.length;
-  const trainingDays = hours.filter((hour) => hour > 0).length;
-  const average = hours.reduce((acc, cur) => acc + cur, 0) / hours.length;
-  const success = average >= target;
-  const rating: 1 | 2 | 3 =
-    average >= target ? 3 : average >= target * 0.5 ? 2 : 1;
-
-  const RATING_DESCRIPTIONS = {
-    1: 'bad',
-    2: 'not too bad but could be better',
-    3: 'good job',
-  };
-  const ratingDescription = RATING_DESCRIPTIONS[rating];
-
-  return {
-    periodLength,
-    trainingDays,
-    success,
-    rating,
-    ratingDescription,
-    target,
-    average,
-  };
 };
 
 try {
--- exercises/index.ts
+++ exercises/index.ts
@@ -1,34 +1,53 @@
 import express from 'express';
 import calculateBmi from './calculateBmi';
+import calculateExercises from './calculateExercises';
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
 app.get('/bmi', (req, res) => {
-  try {
-    const height = Number(req.query.height);
-    const weight = Number(req.query.weight);
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
-    if (isNaN(height) || isNaN(weight)) {
-      throw new Error('malformatted parameters');
-    }
-
-    const bmi = calculateBmi(height, weight);
-
-    const bmiResult = {
-      height,
-      weight,
-      bmi,
-    };
-
-    res.json(bmiResult);
-  } catch (e) {
-    if (e instanceof Error) {
-      res.status(400).json({ error: e.message });
-    }
+  if (isNaN(height) || isNaN(weight)) {
+    return res.status(400).json({ error: 'malformatted parameters' });
   }
+
+  const bmi = calculateBmi(height, weight);
+
+  const bmiResult = {
+    height,
+    weight,
+    bmi,
+  };
+
+  return res.json(bmiResult);
+});
+
+interface ExercisesReqBody {
+  target: number;
+  daily_exercises: Array<number>;
+}
+
+app.post('/exercises', (req, res) => {
+  const { target, daily_exercises: hours } = req.body as ExercisesReqBody;
+
+  if (!target || !hours) {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+
+  if (isNaN(Number(target)) || hours.some((hour) => isNaN(Number(hour)))) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const result = calculateExercises(target, hours);
+
+  return res.json(result);
 });
 
 const PORT = 3002;
