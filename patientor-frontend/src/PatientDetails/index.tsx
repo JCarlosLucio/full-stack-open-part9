61abe39eeb3f6dfa99f0f64c0759e665877eb6b8
jcLucio <c.lucio3@gmail.com>
2021-08-03 10:53:55 -0500

    Add patientor step2 exercise (9.17) - Add GenderIcon / Entry types - Add entries prop to Patient type - Update patient name in PatientListPage to be Link to /patients/:id - Add Route to /patients/:id - Add UPDATE_PATIENT to reducer / Action - Add PatientDetails component

---
+++ patientor-frontend/src/PatientDetails/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { Header, Icon, List } from 'semantic-ui-react';
+
+import { apiBaseUrl } from '../constants';
+import { useStateValue } from '../state';
+import { Gender, GenderIcon, Patient } from '../types';
+
+const PatientDetails = () => {
+  const [{ patients }, dispatch] = useStateValue();
+  const { id } = useParams<{ id: string }>();
+
+  const patient = patients[id];
+
+  useEffect(() => {
+    const fetchPatient = async () => {
+      try {
+        const { data: patientFromApi } = await axios.get<Patient>(
+          `${apiBaseUrl}/patients/${id}`
+        );
+        dispatch({ type: 'UPDATE_PATIENT', payload: patientFromApi });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+
+    if (!patient || !patient?.ssn) {
+      void fetchPatient();
+    }
+  }, []);
+
+  const getGenderIcon = (gender: Gender): GenderIcon => {
+    switch (gender) {
+      case Gender.Male:
+        return GenderIcon.Male;
+      case Gender.Female:
+        return GenderIcon.Female;
+      default:
+        return GenderIcon.Other;
+    }
+  };
+
+  if (!patient) {
+    return null;
+  }
+
+  return (
+    <div>
+      <Header size="large">
+        <Header.Content>
+          {patient.name}
+          <Icon name={getGenderIcon(patient.gender)} />
+        </Header.Content>
+      </Header>
+      <List size="large">
+        <List.Item>ssn: {patient.ssn}</List.Item>
+        <List.Item>occupation: {patient.occupation}</List.Item>
+      </List>
+    </div>
+  );
+};
+
+export default PatientDetails;
--- patientor-frontend/src/types.ts
+++ patientor-frontend/src/types.ts
@@ -5,10 +5,19 @@ export interface Diagnosis {
 }
 
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+  Male = 'male',
+  Female = 'female',
+  Other = 'other',
 }
+
+export enum GenderIcon {
+  Male = 'mars',
+  Female = 'venus',
+  Other = 'other gender',
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {}
 
 export interface Patient {
   id: string;
@@ -17,4 +26,5 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries?: Entry[];
 }
