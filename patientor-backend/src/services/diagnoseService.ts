54921d6148f40c4cfd840237fd29372028e79474
jcLucio <c.lucio3@gmail.com>
2021-08-03 16:37:47 -0500

    Add patientor step4 exercise (9.19) - Rename Diagnose type to Diagnosis - Update diagnoses data to use Diagnosis type - Rename diagnoseService.ts to diagnosisService.ts - Update diagnoses route to use diagnosisService - Rename diagnoseRouter to diagnosisRouter in app - Add HealthCheckRating / HealthCheckEntry types - Add Discharge / HospitalEntry types - Add SickLeave / OccupationalHealthcareEntry types - Add BaseEntry / Entry types - Update patients data with entries

--- patientor-backend/src/services/diagnoseService.ts
+++
@@ -1,6 +0,0 @@
-import diagnoses from '../../data/diagnoses';
-import { Diagnose } from '../types';
-
-const getDiagnoses = (): Diagnose[] => diagnoses;
-
-export default { getDiagnoses };
---
+++ patientor-backend/src/services/diagnosisService.ts
@@ -0,0 +1,6 @@
+import diagnoses from '../../data/diagnoses';
+import { Diagnosis } from '../types';
+
+const getDiagnoses = (): Diagnosis[] => diagnoses;
+
+export default { getDiagnoses };
